8d92cab5aea2887d2e350bd01bac2490e8ec7b44
Nick Foden <nickfoden@gmail.com>
2019-07-31 10:19:43 -0400

    Simple example test started with testing library

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { cleanup, render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/react/cleanup-after-each";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+afterEach(cleanup);
+
+const TestComponent = () => <div>hello</div>;
+
+test("react-testing-library test", () => {
+  const { getByText } = render(<TestComponent />);
+  const hello = getByText("hello");
 });
